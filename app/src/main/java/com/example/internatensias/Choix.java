8c6569a9fe6d4ede4e40fbb328625c2a36d2df40
nadas123x <77413627+nadas123x@users.noreply.github.com>
2022-04-12 02:55:38 +0200

    ok

--- app/src/main/java/com/example/internatensias/Choix.java
+++ app/src/main/java/com/example/internatensias/Choix.java
@@ -10,11 +10,13 @@ import android.widget.Button;
 public class Choix extends AppCompatActivity {
 Button btnvisiteur;
 Button button2;
+Button btnAdmin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choix
         );
+        btnAdmin=findViewById(R.id.btnAdmin);
         btnvisiteur=findViewById(R.id.btnvisiteur);
         button2=findViewById(R.id.button2);
         btnvisiteur.setOnClickListener(new View.OnClickListener() {
@@ -24,12 +26,14 @@ Button button2;
 
             }
 
-
-
         });
         button2.setOnClickListener(view -> {
             startActivity(new Intent(Choix.this, login.class));
         });
+        btnAdmin.setOnClickListener(view -> {
+            startActivity(new Intent(Choix.this, PageAdmin.class));
+        });
+
 
     }
 }
