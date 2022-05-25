430f35268790785b97729fda256e59ccbc4d8efc
nadas123x <77413627+nadas123x@users.noreply.github.com>
2022-05-25 23:42:15 +0200

    ok

--- app/src/main/java/com/example/internatensias/PageAdmin.java
+++ app/src/main/java/com/example/internatensias/PageAdmin.java
@@ -7,15 +7,28 @@ import android.os.Bundle;
 import android.widget.Button;
 
 public class PageAdmin extends AppCompatActivity {
-Button btnAdminResidents;
+Button btnAdminResidents,btnAdminDemandes,btnAdminProblemes,logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page_admin);
         btnAdminResidents=findViewById(R.id.btnAdminResidants);
+        btnAdminDemandes=findViewById(R.id.btnAdminDemandes);
+        btnAdminProblemes=findViewById(R.id.btnAdminProblemes);
+        logout=findViewById(R.id.logout);
+
         btnAdminResidents.setOnClickListener(view -> {
             startActivity(new Intent(PageAdmin.this, MenuAdmResident.class));
+        });
+        btnAdminDemandes.setOnClickListener(view -> {
+            startActivity(new Intent(PageAdmin.this, MainActivityDemande.class));
+        });
+        btnAdminProblemes.setOnClickListener(view -> {
+            startActivity(new Intent(PageAdmin.this, ListeProblemes.class));
+        });
+        logout.setOnClickListener(view -> {
+            startActivity(new Intent(PageAdmin.this, LoginAdmin.class));
         });
     }
 }
